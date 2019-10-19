06ef00e616a260811d5bd6788b98475dc141cfe7
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2019-10-19 22:41:08 +0800

    Update object.h
    Append class _err,class _bool and fix some errors.

--- FxVM/object.h
+++ FxVM/object.h
@@ -1,16 +1,18 @@
 #include<vector>
 #include<map>
 #include<string>
+class _err;
+class _bool;
 class __object{
   public:
     virtual std::string type(){
       return "?";
     }
     virtual __object * calc(std::string _operator){
-      return this;
+      return (new _err)->setval(_operator+this->type()+" is undefined.");
     }
     virtual __object * calc(std::string _operator,__object * another){
-      return this;
+      return (new _err)->setval(this->type()+_operator+another->type()+" is undefined.");
     }
 }
 class object{
@@ -26,6 +28,10 @@ class object{
     std::string type(){
       return this->val->type();
     }
+    object setval(__object * val){
+      delete this->val;
+      this->val=val;
+    }
     object calc(std::string _operator){
       this->val=this->val->calc(_operator);
       return *this;
@@ -33,6 +39,76 @@ class object{
     object calc(std::string _operator,object another){
       this->val=this->val->calc(_operator,another.val);
       return *this;
+    }
+}
+class _err:__object{
+  private:
+    std::string val;
+  public:
+    std::string type(){
+      return "_err";
+    }
+    _err * setval(std::string val){
+      this->val=val;
+      return this;
+    }
+    __object * calc(std::string _operator){
+      if(_operator=="?"){
+        return (new _bool)->setval(true);
+      }else{
+        return (new _err)->setval(_operator+this->type()+" is underfined.");
+      }
+    }
+    __object * calc(std::string _operator,__object * another){
+      _err * tmp=dynamic_cast<_err *>(another);
+      if(tmp!=nullptr){
+        if(_operator=="+"){
+          return (new _err)->setval(this->val+"\n\n"+tmp->val);
+        }else if(_ooperator=="="){
+          return (new _bool)->setval(this->val==tmp->val);
+        }else{
+          return this->calc("+",tmp)->calc("+",(new _err)->setval(this->type()+_operator+tmp->type()+" is underfined.");
+        }
+      }else{
+        return this->calc("+",(new _err)->setval(this->type()+_operator+tmp->type()+" is underfined.");
+      }
+    }
+}
+class _bool:__object{
+  private:
+    bool val;
+  public:
+    std::string type(){
+      return "_bool";
+    }
+    _bool * setval(bool val){
+      this->val=val;
+      return this;
+    }
+    __object * calc(std::string _operator){
+      if(_operator=="-"){
+        return (new _bool)->setval(!(this->val));
+      }else if(_operator=="?"){
+        return (new _bool)->setval(true);
+      }else{
+        return (new _err)->setval(_operator+this->type()+" is undefined.");
+      }
+    }
+    __object * calc(std::string _operator,__object * another){
+      _bool * tmp=dynamic_cast<_bool *>(another);
+      if(tmp!=nullptr){
+        if(_operator=="="){
+          return (new _bool)->setval(this->val==tmp->val);
+        }else if(_operator=="/\"){
+          return (new _bool)->setval(this->val&&tmp->val);
+        }else if(_operator=="\/"){
+          return (new _bool)->setval(this->val||tmp->val);
+        }else{
+          return (new _err)->setval(this->type()+_operator+tmp->type()+" is underfined.");
+        }
+      }else{
+        return (new _err)->setval(this->type()+_operator+tmp->type()+" is undefined.");
+      }
     }
 }
 class _num:__object{
@@ -48,32 +124,31 @@ class _num:__object{
     }
     __object * calc(std::string _operator){
       if(_operator=="-"){
-        this->val=-(this->val);
-        return this;
+        return (new _num)->setval(-(this->val));
       }else if(_opertor=="?"){
-        return (new _bool).setval(true);
+        return (new _bool)->setval(true);
       }else{
-        return (new _err).setval(_operator+this->type()+" is underfined.");
+        return (new _err)->setval(_operator+this->type()+" is underfined.");
       }
     }
     __object * calc(std::string _operator,__object * another){
       _num * tmp=dynamic_cast<_num *>(another);
       if(tmp!=nullptr){
         if(_operator=="^"){
-          return (new _num).setval(pow(this->val,tmp->val));
+          return (new _num)->setval(pow(this->val,tmp->val));
         }else if(_operator=="*"){
-          return (new _num).setval(this->val*(tmp->val));
+          return (new _num)->setval(this->val*(tmp->val));
         }else if(_operator=="/"){
-          return (new _num).setval(this->val/(tmp->val));
+          return (new _num)->setval(this->val/(tmp->val));
         }else if(_operator=="+"){
-          return (new _num).setval(this->val+(tmp->val));
+          return (new _num)->setval(this->val+(tmp->val));
         }else if(_operator=="<"){
-          return (new _num).setval(this->val<(tmp->val));
+          return (new _bool)->setval(this->val<(tmp->val));
         }else{
-          return (new _err).setval(this->type()+_operator+tmp->type()+" is undefined.");
+          return (new _err)->setval(this->type()+_operator+tmp->type()+" is undefined.");
         }
       }else{
-        return (new _err).setval(this->type()+_operator+tmp->type()+" is unefined.");
+        return (new _err)->etval(this->type()+_operator+tmp->type()+" is unefined.");
       }
 }
         
