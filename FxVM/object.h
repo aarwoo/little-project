fb5841476c2edf292d3a31ef216eae08488903f4
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2019-10-20 16:58:16 +0800

    Update object.h
    Append class _func and a new virtual function tostr

--- FxVM/object.h
+++ FxVM/object.h
@@ -1,5 +1,5 @@
 #include<vector>
-#include<map>
+#include<unordered_map>
 #include<string>
 class _err;
 class _bool;
@@ -7,6 +7,9 @@ class __object{
   public:
     virtual std::string type(){
       return "?";
+    }
+    virtual std::string tostr(){
+      return "";
     }
     virtual __object * calc(std::string _operator){
       return (new _err)->setval(_operator+this->type()+" is undefined.");
@@ -28,6 +31,9 @@ class object{
     std::string type(){
       return this->val->type();
     }
+    std::string tostr(){
+      return this->val->tostr();
+    }
     object setval(__object * val){
       delete this->val;
       this->val=val;
@@ -47,6 +53,9 @@ class _err:__object{
   public:
     std::string type(){
       return "_err";
+    }
+    std::string tostr(){
+      return this->val;
     }
     _err * setval(std::string val){
       this->val=val;
@@ -81,6 +90,13 @@ class _bool:__object{
     std::string type(){
       return "_bool";
     }
+    std::string tostr(){
+      if(this->val==true){
+        return "_true";
+      else{
+        return "_false";
+      }
+    }
     _bool * setval(bool val){
       this->val=val;
       return this;
@@ -111,12 +127,53 @@ class _bool:__object{
       }
     }
 }
+class _str:__object{
+  private:
+    std::string val;
+  public:
+    std::string type(){
+      return "_err";
+    }
+    std::string tostr(){
+      return this->val;
+    }
+    _str * setval(std::string val){
+      this->val=val;
+      return this;
+    }
+    __object * calc(std::string _operator){
+      if(_operator=="?"){
+        return (new _bool)->setval(true);
+      }else{
+        return (new _err)->setval(_operator+this->type()+" is underfined.");
+      }
+    }
+    __object * calc(std::string _operator,__object * another){
+      _str * tmp=dynamic_cast<_str *>(another);
+      if(tmp!=nullptr){
+        if(_operator=="+"){
+          return (new _str)->setval(this->val+tmp->val);
+        }else if(_operator=="<"){
+          return (new _bool)->setval(this->val<tmp->val);
+        }else{
+          return (new _err)->setval(this->type()+_operator+tmp->type()+" is underfined.");
+        }
+      }else{
+        return (new _err)->setval(this->type()+_operator+tmp->type()+" is underfined.");
+      }
+    }
+}
 class _num:__object{
   private:
     long double val;
   public:
     std::string type(){
       return "_num";
+    }
+    std::string tostr(){
+      std::stringstream tmp;
+      tmp<<this->val;
+      return tmp.str();
     }
     _num * setval(long double val){
       this->val=val;
@@ -150,5 +207,66 @@ class _num:__object{
       }else{
         return (new _err)->etval(this->type()+_operator+tmp->type()+" is unefined.");
       }
+    }
 }
-        
+class _func:__object{
+  private:
+    std::vector<std::string> argument_var;
+    std::vector<pair<expression,expression> > expr;
+  public:
+    std::string type(){
+      return "_func";
+    }
+    std::string tostr(){
+      std::stringstream tmp;
+      size_t i;
+      tmp<<"(";
+      for(i=0;i<this->argument_var.size()-1;i=i+1){
+        tmp<<(this->argument_var)[i]<<",";
+      }
+      tmp<<(this->argument_var)[i]<<")=>{";
+      for(i=0;i<this->expr.size()-1;i=i+1){
+        if((this->expr)[i].first().empty()==false){
+          tmp<<(this->expr)[i].first().tostr()<<":"<<(this->expr)[i].second().tostr()<<",\n";
+        }else{
+          tmp<<(this->expr)[i].second().tostr()<<",\n";
+        }
+      }
+      if((this->expr)[i].first().empty()==false){
+        tmp<<(this->expr)[i].first().tostr()<<":"<<(this->expr)[i].second().tostr()<<"}";
+      }else{
+        tmp<<(this->expr)[i].second().tostr()<<"}";
+      }
+      return tmp.str();
+    }
+    _func * setval(std::vector<std::string> argument_var,std::vector<std::pair<expression,expression> > expr){
+      this->argument_var=argument_var;
+      this->expr=expr;
+      return this;
+    }
+    object apply(std::vector<object> argument){
+      std::unordered_map<std::string,object> map;
+      std::stringstream tmp;
+      object tmp1;
+      if(this->argument_var.size()==argument.size()){
+        for(size_t i=0;i<argument.size();i=i+1){
+          map[(this->argument_var)[i]]=argument[i];
+        }
+        for(auto i:this->expr){
+          if(i.first().empty()==false){
+            tmp1=i.first().eval(map);
+          }else{
+            tmp1=tmp1.setval((new _bool)->setval(true));
+          }
+          if(tmp1.type()=="_bool"&&tmp1.tostr()=="_true"){
+            return i.second().eval(map);
+          }else{
+            /*PASS*/
+          }
+        }
+      }else{
+        tmp<<this->tostr()<<" must receive and only receive "<<this->argument_var.size()<<" arguments.";
+        return (new _err)->setval(tmp.str());
+      }
+    }
+}
