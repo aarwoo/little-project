2ba998d3668b4a4c6e0f9f6800f7b6cef66fbfd5
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2019-10-20 22:20:58 +0800

    Update expression.h

--- FxVM/expression.h
+++ FxVM/expression.h
@@ -1,10 +1,11 @@
 #ifndef expression_h
 #define expression_h
 #include"object.h"
+#include"generator.h"
 class expression{
   private:
     std::string _operator;
-    std::string id;
+    generator gen;
     object val;
     expression * l_subexpr;
     expression * r_subexpr;
@@ -18,15 +19,15 @@ class expression{
       delete this->r_subexpr;
     }
     bool empty(){
-      return this->_operator==""&&this->id==""&&this->val.type()=="?"&&this->l_subexpr==nullptr&&this->r_subexpr==nullptr;
+      return this->_operator==""&&this->gen.empty()==true&&this->val.type()=="?"&&this->l_subexpr==nullptr&&this->r_subexpr==nullptr;
     }
-    expression setval(]std::string src_expr){
+    expression setval(std::string src_expr){
       
     }
     std::string tostr(){
       if(this->l_subexpr==nullptr){
         if(this->r_subexpr==nullptr){
-          return this->id+this->val.tostr();
+          return this->gen.tostr()+this->val.tostr();
         }else{
           return "("+this->_operator+this->r_subexpr->tostr()+")";
         }
