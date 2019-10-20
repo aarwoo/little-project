5f3c3f54c1ef2a513adf92658324a0d8712a6fda
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2019-10-20 21:48:37 +0800

    Update expression.h

--- FxVM/expression.h
+++ FxVM/expression.h
@@ -20,14 +20,19 @@ class expression{
     bool empty(){
       return this->_operator==""&&this->id==""&&this->val.type()=="?"&&this->l_subexpr==nullptr&&this->r_subexpr==nullptr;
     }
+    expression setval(]std::string src_expr){
+      
+    }
     std::string tostr(){
       if(this->l_subexpr==nullptr){
         if(this->r_subexpr==nullptr){
-          if(this->_operator==""){
-            return this->id+val.tostr();
-          }else{
-            return "("+this->_operator+this->id+val.tostr+")";
-          }
+          return this->id+this->val.tostr();
+        }else{
+          return "("+this->_operator+this->r_subexpr->tostr()+")";
+        }
+      }else{
+        return "("+this->l_subexpr->tostr()+this->_operator+this->r_subexpr->tostr()+")";
+      }
     }
     object eval(std::unordered_map<std::string,object> map){
       
