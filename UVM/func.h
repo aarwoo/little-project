8a4c8aef0f74d83c3b77fd8d0046fce0e432b047
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2019-12-26 11:56:46 +0800

    Update func.h

--- UVM/func.h
+++ UVM/func.h
@@ -1,8 +1,43 @@
-#include<unordered_map>
+#include<string>
+#include<vector>
+#include"object.h"
 class func{
   private:
-    std::unordered_map<string,func>var;
+    std::string var;
+    object atom;
     std::vector<func>expr;
+    func set_var(std::string _var,func vexpr){
+      if(_var==var){
+        if(expr.size()==0){
+          expr.push_back(vexpr);
+          var="";
+        }else{
+          /*Pass*/
+        }
+      }else{
+        for(auto& i:expr){
+          i=i.setvar(_var,vexpr);
+        }
+      }
+      return *this;
+    }
   public:
-    func as_var(string _var,func _expr);
+    func apply(func vexpr){
+      if(var!=""){
+        for(auto& i:expr){
+          i=i.setvar(var,vexpr);
+        }
+      }else{
+        atom=atom.apply(vexpr);
+      }
+    return *this;
+    }
+    func calc(){
+      func ans;
+      expr[0]=expr[0].calc();
+      ans=expr[0];
+      for(size_t i=1;i<expr.size();i=i+1){
+        ans=ans.apply(expr[i]).calc();
+      }
+      return ans;
 };
