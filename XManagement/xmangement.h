a8669a4d9587171415c5a8f81bf400bd2eba0217
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2020-01-01 00:23:43 +0800

    Update xmangement.h

--- XManagement/xmangement.h
+++ XManagement/xmangement.h
@@ -5,11 +5,17 @@ uchar** vals;
 size_t size;
 xmangement * next;
 }xmangement;
-xmangement new_xmangement(uchar**keys,size_t size){
+xmangement new_xmangement(uchar** keys,size_t size,size_t max_len){
   xmangement ret;
   ret.next=NULL;
   ret.size=size;
   ret.vals=(uchar**)calloc(size,sizeof(uchar*));
   if(ret.vals!=NULL){
+    size_t len;
     for(size_t i=0;i<size;i=i+1){
-      *(ret.vals+i)=(uchar**)calloc(
+      len=ustrnlen(*(keys+i),max_len);
+      *(ret.vals+i)=(uchar*)calloc(len+1,sizeof(uchar));
+      if(*(ret.vals+i)!=NULL){
+        ustrcpy(*(ret.vals+i),len+1,*(keys+i));
+      }else{
+        
