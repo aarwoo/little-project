ca4a3e0da4bb646a96ac219bd2f38422ea1f4c6b
master aarwoo <51515401+aarwoo@users.noreply.github.com>
2020-02-15 12:12:53 +0800

    Update func.h
    C++ shared_ptr has some hole that you may jmp in

--- UVM/func.h
+++ UVM/func.h
@@ -1,12 +1,39 @@
+//This code is for C++20
 #include<string>
 #include<vector>
+#include<unordered_map>
+#include<memory>
 class func{
   private:
     std::string obj;
+    std::shared_ptr<func> above;
+    std::shared_ptr<std::unordered_map<std::string,func> > space;
     std::vector<func> expr;
-    func set_var(std::string & obj,func & f){
-    }
   public:
-    func apply(func f);
-    func calc();
+    func func(){
+      space=std::make_shared_default_init<std::unordered_map<std::string,func> >();
+    }
+    func& look_up_space(std::string key){
+      if(space->contains(key)==true){
+        return space[func];
+      }else if(above.get()!=nullptr){
+        return above->look_up_space(key);
+      }else{
+        return space[func];
+    func calc(){
+      if(expr.size()==0){
+         if(obj[0]=="$"){
+           return Kernel(obj.substr(1));
+         }else if(obj[0]=="#"){
+           func tmp;
+           tmp.obj=obj.substr(1);
+           tmp.above=above;
+           tmp.space=space;
+           return tmp;
+         }else if(obj[0]=="\""||obj[0]=="+"){
+           return *this;
+         }else{
+           return look_up_space(obj);
+         }
+           
 };
